45ae9b02440a4154d213e76848dde5167a1da8dc
NamanJParikh <namanjparikh@gmail.com>
2024-07-24 09:46:54 -0400

    log changes

--- streaming_scripts/watchdog_processor.py
+++ streaming_scripts/watchdog_processor.py
@@ -16,6 +16,7 @@ from watchdog.events import (
 )
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+import temperature_analysis
 
 ########## Setup ##########
 
@@ -23,18 +24,18 @@ from watchdog.observers import Observer
 CONSUMER_TOPIC_NAME = "hyperspec_LDFZ_data"
 TOPIC_NAME = "hyperspec_LDFZ_result"
 
-root_dir = pathlib.Path("/home/nparik15/")
-CONFIG_FILE_PATH = root_dir / "config_files" / "paradim01_broker.config"
-RECO_DIR = root_dir / "hyperspec_LDFZ_data"
+# root_dir = pathlib.Path("/home/nparik15/")
+# CONFIG_FILE_PATH = root_dir / "config_files" / "paradim01_broker.config"
+# RECO_DIR = root_dir / "hyperspec_LDFZ_data"
 
-# # Path to the root directory of this repo
-# repo_root_dir = pathlib.Path().resolve().parent
+# Path to the root directory of this repo
+repo_root_dir = pathlib.Path().resolve().parent
 
-# # Paths to the config file and the directory holding the test files
-# CONFIG_FILE_PATH = repo_root_dir / "streaming_scripts" / "config_files" / "paradim01_broker.config"
+# Paths to the config file and the directory holding the test files
+CONFIG_FILE_PATH = repo_root_dir / "streaming_scripts" / "config_files" / "paradim01_broker.config"
 
-# # Path to the directory to store the reconstructed data
-# RECO_DIR = repo_root_dir / "streaming_scripts" / "image_reco"
+# Path to the directory to store the reconstructed data
+RECO_DIR = repo_root_dir / "streaming_scripts" / "image_reco"
 
 # # Path to the director to store temperature arrays resulting from analysis
 # ANALYSIS_DIR = repo_root_dir / "streaming_scripts" / "processor_1"
