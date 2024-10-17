595d8f88dcced762764ae91e152be60666c7fcbb
NamanJParikh <namanjparikh@gmail.com>
2024-10-16 23:43:13 -0400

    Debugging vectorized analysis - still buggy

---
+++ StreamingScripts/.ipynb_checkpoints/hyperspec-checkpoint.py
@@ -0,0 +1,154 @@
+########## Imports ##########
+
+import numpy as np, matplotlib, matplotlib.pyplot as plt
+from io import BytesIO
+import pathlib, importlib, logging, datetime, json, platform
+from threading import Thread
+from openmsitoolbox.logging import OpenMSILogger
+from openmsistream import (
+    UploadDataFile,
+    DataFileUploadDirectory,
+    DataFileDownloadDirectory,
+    DataFileStreamProcessor,
+    MetadataJSONReproducer,
+)
+
+
+
+########## Setup ##########
+
+matplotlib.use("Agg")
+
+# The name of the topic to work with
+TOPIC_NAME = "hyperspec_LDFZ_data"
+# The name of the topic to consume files from
+CONSUMER_TOPIC_NAME = "hyperspec_LDFZ_result"
+
+# root_dir = pathlib.Path("C:/")
+# CONFIG_FILE_PATH = root_dir / "Headwall" / "sensor1" / "broker_configs" / "paradim01_broker.config"
+# TEST_FILE_DIR = root_dir / "Headwall" / "sensor1" / "captured"
+# STREAM_PROCESSOR_OUTPUT_DIR = root_dir / "Headwall" / "sensor1" / "pyrometry_results"
+
+# Paths to the config file and the directory holding the test files
+repo_root_dir = pathlib.Path().resolve().parent
+CONFIG_FILE_PATH = repo_root_dir / "StreamingScripts" / "config_files" / "paradim01_broker.config"
+TEST_FILE_DIR = repo_root_dir / "StreamingScripts" / "test_folder"
+
+# Path to the directory to store the StreamProcessor output
+STREAM_PROCESSOR_OUTPUT_DIR = repo_root_dir / "StreamingScripts" / "processor_2"
+
+
+
+########## Tasks ##########
+
+def upload_task(upload_directory, *args, **kwargs):
+    """Run "upload_files_as_added" for a given DataFileUploadDirectory, and log a message
+    when it gets shut down
+
+    Args:
+        upload_directory (DataFileUploadDirectory): the DataFileUploadDirectory to run
+        args (list): passed through to "upload_files_as_added"
+        kwargs (dict): passed through to "upload_files_as_added"
+    """
+    # This call to "upload_files_as_added" waits until the program is shut down
+    uploaded_filepaths = upload_directory.upload_files_as_added(*args, **kwargs)
+    msg = (
+        f"The following files were uploaded:\n\t"
+    )
+    msg += "\n\t".join([str(fp) for fp in uploaded_filepaths])
+    upload_directory.logger.info(msg)
+
+class PlaceholderStreamProcessor(DataFileStreamProcessor):
+    """Saves the returned temperature array as well as a heatmap plot of it
+    """
+
+    def _process_downloaded_data_file(self, datafile, lock):
+        try:
+            # construct output paths
+            rel_filepath = datafile.relative_filepath
+            rel_fp_str = str(rel_filepath.as_posix()).replace("/","_").replace(".","_")
+            output_filepath = self._output_dir / f"{rel_fp_str}_result.npy"
+
+            with lock:
+                # download the incoming numpy array file
+                with open(output_filepath, "wb") as filep:
+                    filep.write(BytesIO(datafile.bytestring).read())
+
+            # load the array and save a thermal gradient plot
+            temp_arr = np.load(output_filepath, allow_pickle=True)
+            plt.figure()
+            plt.imshow(temp_arr, cmap="hot")
+            plt.colorbar()
+            output_filepath = self._output_dir / f"{rel_fp_str}_remade.png"
+            plt.savefig(output_filepath)
+                
+        except Exception as exc:
+            return exc
+        return None
+    
+    @classmethod
+    def run_from_command_line(cls, args=None):
+        "Not used in this example... stay tuned for the live coding tomorrow!"
+        pass
+
+def stream_processor_task(stream_processor):
+    """Run "process_files_as_read" for the given stream processor, and log a message
+    when it gets shuts down
+    
+    Args:
+        stream_processor (openmsistream.DataFileStreamProcessor): The stream processor to run
+    """
+    # This call to "process_files_as_read" hangs until the stream processor is shut down
+    (
+        n_m_r, # The number of messages read
+        n_m_p, # The number of messages processed
+        n_f_p, # The number of files successfully processed
+        p_fps, # Paths to the most recently-processed files
+    ) = stream_processor.process_files_as_read()
+    stream_processor.close()
+    msg = f"{n_m_r} total messages were consumed"
+    if n_f_p > 0:
+        msg += (
+            f", {n_m_p} messages were processed,"
+            f" and {n_f_p} files were successfully processed"
+        )
+    else:
+        msg += f" and {n_m_p} messages were successfully processed"
+    msg += (
+        f". Up to {stream_processor.N_RECENT_FILES} most recently "
+        "processed files:\n\t"
+    )
+    msg += "\n\t".join([str(fp) for fp in p_fps])
+    stream_processor.logger.info(msg)
+
+
+
+########## Run ##########
+
+# Create the DataFileUploadDirectory
+dfud = DataFileUploadDirectory(TEST_FILE_DIR, CONFIG_FILE_PATH)
+# Start running its "upload_files_as_added" function in a separate thread
+upload_thread = Thread(
+    target=upload_task,
+    args=(
+        dfud,
+        TOPIC_NAME,
+    ),
+)
+
+
+# Create the StreamProcessor
+psp = PlaceholderStreamProcessor(
+    CONFIG_FILE_PATH,
+    CONSUMER_TOPIC_NAME,
+    output_dir=STREAM_PROCESSOR_OUTPUT_DIR,
+)
+# Start running its "process_files_as_read" function in a separate thread
+processor_thread = Thread(
+    target=stream_processor_task,
+    args=(psp,),
+)
+
+if __name__ == "__main__":
+    upload_thread.start()
+    processor_thread.start()
---
+++ StreamingScripts/.ipynb_checkpoints/watchdog_processor-checkpoint.py
@@ -0,0 +1,161 @@
+########## Imports ##########
+
+import numpy as np
+import os, pathlib, importlib, logging, time, datetime, json, platform
+from threading import Thread
+from openmsitoolbox.logging import OpenMSILogger
+from openmsistream import (
+    DataFileDownloadDirectory,
+    DataFileStreamProcessor,
+    MetadataJSONReproducer,
+    UploadDataFile,
+)
+from watchdog.events import (
+    FileCreatedEvent,
+    DirCreatedEvent,
+)
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import temperature_analysis
+
+########## Setup ##########
+
+# The name of the topic to consume files from
+CONSUMER_TOPIC_NAME = "hyperspec_LDFZ_data"
+TOPIC_NAME = "hyperspec_LDFZ_result"
+
+# root_dir = pathlib.Path("/home/nparik15/")
+# CONFIG_FILE_PATH = root_dir / "config_files" / "paradim01_broker.config"
+# RECO_DIR = root_dir / "hyperspec_LDFZ_data"
+
+# Path to the root directory of this repo
+repo_root_dir = pathlib.Path().resolve().parent
+
+# Paths to the config file and the directory holding the test files
+CONFIG_FILE_PATH = repo_root_dir / "StreamingScripts" / "config_files" / "paradim01_broker.config"
+
+# Path to the directory to store the reconstructed data
+RECO_DIR = repo_root_dir / "StreamingScripts" / "image_reco"
+
+# # Path to the director to store temperature arrays resulting from analysis
+# ANALYSIS_DIR = repo_root_dir / "streaming_scripts" / "processor_1"
+
+
+
+########## Tasks ##########
+
+def download_task(download_directory):
+    """Run "reconstruct" for a given DataFileDownloadDirectory, and log some messages
+    when it gets shut down
+
+    Args:
+        download_directory (DataFileDownloadDirectory): the DataFileDownloadDirectory to run
+    """
+    # This call to "reconstruct" waits until the program is shut down
+    (
+        n_read,
+        n_processed,
+        n_complete_files,
+        complete_filepaths,
+    ) = download_directory.reconstruct()
+    download_directory.close()
+    msg = f"{n_read} total messages were consumed"
+    if len(complete_filepaths) > 0:
+        msg += (
+            f", {n_processed} messages were successfully processed, and "
+            f'{n_complete_files} file{" was" if n_complete_files==1 else "s were"} '
+            "successfully reconstructed"
+        )
+    else:
+        msg += f" and {n_processed} messages were successfully processed"
+    msg += (
+        f". Most recent completed files (up to {download_directory.N_RECENT_FILES}):\n\t"
+    )
+    msg += "\n\t".join([str(filepath) for filepath in complete_filepaths])
+    download_directory.logger.info(msg)
+
+class Watcher:
+    def __init__(self, path, handler):
+        self.observer = Observer()
+        self.watch_directory = path
+        self.handler = handler
+
+    def run(self):
+        self.observer.schedule(self.handler, self.watch_directory, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(3)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+ 
+        self.observer.join()
+
+# def analysis(folder_path): return np.array([[1, 2], [3, 4], [5, 6]])
+
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_created(event):
+        if event.src_path[-4:] == ".npy" or event.src_path[-4:] == ".log":
+            return
+        if isinstance(event, DirCreatedEvent):
+            print(f"Watchdog found {event.src_path} directory created...")
+            rootdir = event.src_path
+            print(f"Root directory {rootdir}")
+            files = os.listdir(rootdir)
+            print(f"Files {files}")
+        elif isinstance(event, FileCreatedEvent):
+            print(f"Watchdog handling {event.src_path} file created...")
+            rootdir = os.path.dirname(event.src_path)
+            print(f"Root directory {rootdir}")
+            files = os.listdir(rootdir)
+            print(f"Files {files}")
+        else: return
+
+        foldername = rootdir[rootdir.rfind("/")+1:]
+        print(foldername)
+        output_filepath = rootdir + "/" + foldername + ".npy"
+
+        if not (
+            "whiteReference" in files
+            and "whiteReference.hdr" in files
+            and "darkReference" in files
+            and "darkReference.hdr" in files
+            and "raw" in files
+            and "raw.hdr" in files
+            and "data" in files
+            and "data.hdr" in files
+            and "frameIndex.txt" in files
+            and foldername + ".npy" not in files
+        ): return
+
+        folder_path = str(RECO_DIR / foldername)
+        temp_arr = "FAIL"
+        while type(temp_arr) == str:
+            temp_arr = temperature_analysis.analysis(folder_path)
+        
+        np.save(output_filepath, temp_arr, allow_pickle=True)
+        upload_file = UploadDataFile(pathlib.Path(output_filepath), rootdir=rootdir)
+        upload_file.upload_whole_file(CONFIG_FILE_PATH, TOPIC_NAME)
+
+
+
+########## Run ##########
+
+# Create the DataFileDownloadDirectory
+dfdd = DataFileDownloadDirectory(
+    RECO_DIR,
+    CONFIG_FILE_PATH,
+    CONSUMER_TOPIC_NAME,
+)
+# Start running its "reconstruct" function in a separate thread
+download_thread = Thread(
+    target=download_task,
+    args=(dfdd,),
+)
+
+if __name__ == "__main__": 
+    download_thread.start()
+    watcher = Watcher(RECO_DIR, Handler())
+    watcher.run()
